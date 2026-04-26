329c2ca4521975968fdd42c5043e61982fd729c2
Ritik Shrivastav <114847539+ritikshrivastav9517@users.noreply.github.com>
2026-04-26 13:39:49 +0530

    Change server port from 4000 to 3000

--- Server/app.js
+++ Server/app.js
@@ -1,21 +1,35 @@
 import express from 'express';
 import dbConnect from './src/config/dbConnection.js';
+import route from './src/routes/route.js';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import passport from 'passport'; // Passport ko import karein
+import './src/config/passport-setup.js'; // Apni passport config file ko import karein
 
 dotenv.config();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const app = express();
 
+// Middlewares
+app.use(cors({
+    origin: '*', 
+    //origin: 'http://localhost:5173', 
+    credentials: true,
+}));
 app.use(express.json());
+app.use(cookieParser());
+
+// Passport Middleware ko initialize karein
+app.use(passport.initialize());
+
+// Routes
+app.use('/api/v1', route);
 
 const startServer = async () => {
     try {
-        // console.log("PORT:", process.env.PORT);
-        // console.log("MONGO_URI:", process.env.MONGO_URI);
-        
         await dbConnect();
-        
         app.listen(port, () => {
             console.log(`Server is running on port ${port}`);
         });
@@ -26,3 +40,37 @@ const startServer = async () => {
 };
 
 startServer();
+
+
+
+
+
+
+// import express from 'express';
+// import dbConnect from './src/config/dbConnection.js';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const port = process.env.PORT || 4000;
+// const app = express();
+
+// app.use(express.json());
+
+// const startServer = async () => {
+//     try {
+//         // console.log("PORT:", process.env.PORT);
+//         // console.log("MONGO_URI:", process.env.MONGO_URI);
+        
+//         await dbConnect();
+        
+//         app.listen(port, () => {
+//             console.log(`Server is running on port ${port}`);
+//         });
+//     } catch (error) {
+//         console.error("Error starting server:", error.message);
+//         process.exit(1);
+//     }
+// };
+
+// startServer();
